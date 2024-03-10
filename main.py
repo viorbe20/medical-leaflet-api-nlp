3aee60749ef0612a88a4c9d6d8f0b570cbcd0edb
Jose Luixrax <a21pelajo1@iesgrancapitan.org>
2024-03-10 20:41:51 +0100

    modded route test to a new route 'information'

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template
 import os
-import pandas as pd
-import spacy
-from spacy.matcher import Matcher
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import pyttsx3
-import speech_recognition as sr
-import pyaudio
-import requests
-import re
-import textdistance
-from functions import read_excel, search_number_medicine, speech_to_text, text_to_speech, process_text, get_medicine_info
+
+from functions import speech_to_text, text_to_speech, process_text, get_medicine_info
 
 API_URL = "https://cima.aemps.es/cima/rest/medicamento"
 # https://cima.aemps.es/cima/rest/medicamento?nregistro=1181322002IP
@@ -25,54 +15,12 @@ model_path = os.path.join(script_dir, 'data', 'Medicamentos.xlsx')
     
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        data = request.json
-        text_to_speech("Por favor, indique el nombre del medicamento del cual desea obtener información.")
-        try:
-            user_input = speech_to_text()
-        except Exception as e:
-            return render_template('notFound.html', data={"msg": "No se ha detectado ninguna entrada de voz, recarga la página y vuelve a intentarlo."})
-        
-        if user_input:
-            processed_data = process_text(user_input)
-            print("Texto limpio:", processed_data["cleaned_text"])
-            print("Verbos de acción:", processed_data["action_verbs"])
-            print("Valores numéricos:", processed_data["numeric_values"])
-            print("Agregación sintagmática:", processed_data["noun_phrases"])
-        else:
-            print("No se ha detectado ninguna entrada de voz.")
-
-        data = get_medicine_info(processed_data["noun_phrases"][0])
-        # print(data)
-
-        if data is None:
-            return render_template('notFound.html', data={"msg": "No se ha encontrado información sobre el medicamento solicitado."})
-
-        if "fotos" not in data:
-            data["fotos"] = [{"url": "static/img/medicamento.jpg"}]
-
-        if "docs" not in data or len(data["docs"]) < 2:
-            data["docs"] = [{"url": "notFound"}, {"url": "notFound"}]
-                
-        showable_data = {
-            "nombre": data["nombre"],
-            "receta": data["receta"],
-            "generico": data["generico"],
-            "conduc": data["conduc"],
-            "dosis": data["dosis"],
-            "fichaTec": data["docs"][0]["url"],
-            "prospecto": data["docs"][1]["url"],
-            "imgCaja": data["fotos"][0]["url"],
-        }
-        return render_template('test.html', data=showable_data)
-    else:
-        # Renderiza la página
-        return render_template('home.html')
+    return render_template('home.html')
 
 
-@app.route('/test', methods=['GET'])
-def test():
-    # # Iniciar el diálogo. sin no habla da errro. controlar eso
+@app.route('/information', methods=['GET'])
+def information():
+    # Iniciar el diálogo. sin no habla da errro. controlar eso
     text_to_speech("Por favor, indique el nombre del medicamento del cual desea obtener información.")
     try:
         user_input = speech_to_text()
@@ -99,6 +47,7 @@ def test():
 
     if "docs" not in data or len(data["docs"]) < 2:
         data["docs"] = [{"url": "notFound"}, {"url": "notFound"}]
+        
     showable_data = {
         "nombre": data["nombre"],
         "receta": data["receta"],
@@ -109,17 +58,12 @@ def test():
         "prospecto": data["docs"][1]["url"],
         "imgCaja": data["fotos"][0]["url"],
     }
-    return render_template('test.html', data=showable_data)
+    return render_template('information.html', data=showable_data)
     
 @app.route('/notFound', methods=['GET'])
 def notFound():
     data = {"msg" : "No se ha encontrado información sobre el medicamento solicitado."}
     return render_template('notFound.html', data=data)
-
-@app.route('/information', methods=['GET'])
-def information():
-    return render_template('information.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
